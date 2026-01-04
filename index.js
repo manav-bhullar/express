60760ec2c1718c3a3a803a71e3bd34e9c4dadaf1
Manav Bhullar <manavbhullar341@gmail.com>
2026-01-04 12:27:46 +0530

    feat: initialize express app with basic routes

---
+++ index.js
@@ -0,0 +1,18 @@
+const  express = require('express');
+
+const app = express(); 
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, World!</h1>');
+})
+
+app.get("/about", (req, res) =>{
+    res.send("<h1>About Us</h1>");
+})
+
+app.get("/contact", (req,res) => {
+    res.send("<h1>Contact Us</h1>");
+})
+
+
+app.listen(3000)
